c62b0de2cd396fd11f9831e49505cf63ef41074e
nielsvdmarel <nielsvdmarel@gmail.com>
2017-06-08 09:43:08 +0200

    assets erin, nieuw dak, animaties!

---
+++ Assets/AnimController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+	private Animator anim;
+	[SerializeField]
+	private bool _isOpen;
+	[SerializeField]
+	int wait_time;
+	
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+		anim.SetBool("Open", true);
+		anim.SetBool("Closed",false);
+		_isOpen = true;
+		
+		StartCoroutine (waiter());
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+	
+	
+	
+	IEnumerator waiter()
+	{
+		wait_time = Random.Range (20,30);
+		yield return new WaitForSeconds (wait_time);
+		if (_isOpen == true)
+		{
+			_isOpen = false;
+		}
+		else
+		{
+			_isOpen = true;
+		}
+		checker();
+		StartCoroutine (waiter());
+	}
+
+	void checker()
+	{
+		if(_isOpen == true)
+		{
+			anim.SetBool("Open", true);
+			anim.SetBool("Closed",false);
+		}
+		
+		if(_isOpen == false)
+		{
+			anim.SetBool("Open", false);
+			anim.SetBool("Closed",true);
+		}
+	}
+}
+
+
+
